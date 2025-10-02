67b5b1b492405fd5e81efad918ae55d8a41acf4f
Atikur Rahman <fulldevatikur@gmail.com>
2025-10-03 02:23:10 +0600

    updateupdate

--- src/components/GuestHome/ChatVeiw.jsx
+++ src/components/GuestHome/ChatVeiw.jsx
@@ -2,16 +2,15 @@
 import React from "react";
 import UserMsg from "./Chat/UserMsg";
 
-export default function ChatView({ chatBoxHeight, msgs }) {
+export default function ChatView({ chatBoxHeieht, msgs }) {
   return (
     <main
-      style={{ marginBottom: `${chatBoxHeight}px` }}
+      style={{ marginBottom: `${chatBoxHeieht}px` }}
       className="pt-5 w-full flex justify-end items-end overflow-x-hidden touch-pan-y overflow-y-auto"
     >
       <section className="w-[50%] px-8 h-auto flex flex-col gap-10">
         {msgs.map((msg, index) => (
           <>
-            <UserMsg msg={msg.prompt} />
             <UserMsg msg={msg.prompt} />
           </>
         ))}
