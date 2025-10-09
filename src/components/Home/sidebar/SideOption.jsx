0e2d8dacb616ab2a1675479ef83c3bbaa15874f7
Atikur Rahman <fulldevatikur@gmail.com>
2025-10-09 20:35:19 +0600

    something

--- src/components/Home/sidebar/SideOption.jsx
+++ src/components/Home/sidebar/SideOption.jsx
@@ -32,8 +32,8 @@ const SideOption = ({ optionRef, showOption, x, y }) => {
           style={style}
         >
           <ul className="w-full flex flex-col gap-1 py-2 px-2">
-            <Link to={"/account"}>
-              <li className="flex items-center gap-2 hover:bg-[#1f1f22] px-4 py-1.5 rounded-xl text-white cursor-pointer">
+            <Link disabled={true} to={"/account"}>
+              <li className="flex items-center gap-2 hover:bg-[#1f1f22] px-4 py-1.5 rounded-xl text-white cursor-pointer disabled:opacity-75">
                 <User size={18} />
                 <span className="text-sm">Account</span>
               </li>
